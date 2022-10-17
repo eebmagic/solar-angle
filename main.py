703247379e88b3ff637ca504715d56ee9e6b0abc
E Bolton <eebmagic@gmail.com>
2022-10-16 22:24:27 -0500

    changes in main.py

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def datetime_range(start, stop, delta):
     yield curr
     curr += delta
 
+azirange = (np.radians(61.8), np.radians(24.2))
+
 lat = 33.770284 
 lon = -84.393383
 
-start = datetime(2022, 8, 14)
-stop = datetime(2023, 8, 14)
-# timestep = timedelta(minutes=5)
-timestep = timedelta(hours=3)
+# start = datetime(2022, 8, 14)
+start = datetime.now()
+# stop = datetime(2023, 8, 14)
+stop = datetime(2022, 8, 15)
+timestep = timedelta(minutes=30)
 
 dates = [d for d in datetime_range(start, stop, timestep)]
 
@@ -26,10 +29,30 @@ timesDF = pd.DataFrame({
 })
 
 positions = pd.DataFrame(get_position(timesDF['date'], timesDF['lon'], timesDF['lat']))
-azmin, azmax = positions['azimuth'].min(), positions['azimuth'].max()
-almin, almax = positions['altitude'].min(), positions['altitude'].max()
+AZI = positions['azimuth']
+ALT = positions['altitude']
 
-print(positions)
-print(f'azimuth ranges: {np.degrees(azmin), np.degrees(azmax)}')
-print(f'alt ranges: {np.degrees(almin), np.degrees(almax)}')
+# X = np.cos(AZI)
+# Y = np.sin(AZI)
+# Z = np.sin(ALT)
+X = np.degrees(AZI)
+Y = np.degrees(ALT)
+Z = np.arange(0, len(AZI), 1)
 
+# print(positions)
+for i in range(len(AZI)):
+  print(dates[i], np.degrees(AZI[i]), np.degrees(ALT[i]))
+print(f'azimuth ranges: {np.degrees(AZI.min()), np.degrees(AZI.max())}')
+print(f'alt ranges: {np.degrees(ALT.min()), np.degrees(ALT.max())}')
+
+
+print(len(X))
+input('display graph?:')
+import matplotlib.pyplot as plt
+# plt.scatter(AZI, ALT, cmap='gray')
+
+ax = plt.axes(projection='3d')
+ax.scatter3D(X, Y, Z)
+# ax.scatter3D(X, Y, Z+len(AZI)*2, color='green')
+
+plt.show()
